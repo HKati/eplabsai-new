b60c6d0bf87070b5381b57f09793fba149923640
HKati <128643840+HKati@users.noreply.github.com>
2025-04-12 12:12:03 +0200

    js új pro

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// Popup megnyitása
+document.getElementById('open-btn').addEventListener('click', function() {
+  const overlay = document.getElementById('overlay');
+  overlay.classList.remove('hidden');
+  overlay.classList.add('show');
+});
+
+// Popup bezárása
+document.getElementById('close-btn').addEventListener('click', function() {
+  const overlay = document.getElementById('overlay');
+  overlay.classList.remove('show');
+  overlay.classList.add('hidden');
+});
 
