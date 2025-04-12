80e67d21bae794f14f6dff0dd63e8099ed7751e1
HKati <128643840+HKati@users.noreply.github.com>
2025-04-12 11:38:27 +0200

    törlés

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-document.getElementById('open-btn').addEventListener('click', function() {
-    document.getElementById('overlay').classList.add('show');
-    document.getElementById('overlay').classList.remove('hidden');
-});
+
